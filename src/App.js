56a551f8739df847dc9878e90c396650a9dece6f
Cedric Johnson <cjohns0913@gmail.com>
2020-10-13 20:34:12 -0500

    Remove unnecessary stuff; build out homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component'
 
 function App() {
   return (
     <div>
-      Hola, Mundo!
+      <HomePage/>
     </div>
   );
 }
